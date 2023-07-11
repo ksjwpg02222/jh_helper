ab3384156867083c06a643d742e514e521d44430
ksjwpg02222 <ksjwpg02222@infopro.tw>
2023-07-11 18:00:22 +0800

    fix

--- commands/fun/regear.js
+++ commands/fun/regear.js
@@ -114,20 +114,18 @@ module.exports = {
             await interaction.deleteReply()
             await i.reply({ embeds: [exampleEmbed], ephemeral: true });
 
-            for (let i = 0; i < selection.length; i++) {
+            for (let index = 0; index < selection.length; index++) {
                 try {
-                    await CreateRegearEventIdFunc(selection[i])
+                    await CreateRegearEventIdFunc(selection[index])
 
-                    await pushData(selection[i])
-                    continue
+                    await pushData(selection[index])
                 }
                 catch (error) {
                     if (error.name === 'SequelizeUniqueConstraintError') {
-                        await i.followUp({ content: `https://albiononline.com/killboard/kill/${selection[i]}?server=live_sgp 補裝紀錄已存在`, ephemeral: true })
+                        await i.followUp({ content: `https://albiononline.com/killboard/kill/${selection[index]}?server=live_sgp 補裝紀錄已存在`, ephemeral: true })
                     } else {
                         await i.followUp({ content: `發生未知錯誤、請找管理員。`, ephemeral: true })
                     }
-                    continue
                 }
             }
 
